4d32cbcb3bc62ab8e1f9888ec2c933d848daa9eb
Antonio Vanegas <hpsaturn@gmail.com>
2016-12-20 22:19:51 -0500

    add memorymap constants

---
+++ app/src/main/java/admobilize/matrix/gt/SensorBase.java
@@ -0,0 +1,29 @@
+package admobilize.matrix.gt;
+
+/**
+ * Created by Antonio Vanegas @hpsaturn on 12/20/16.
+ */
+
+public abstract class SensorBase {
+
+
+    public Wishbone wb;
+
+    public SensorBase(Wishbone wb) {
+        this.wb = wb;
+    }
+
+    // FPGA Wishbone address map
+    public int kMicrophoneArrayBaseAddress = 0x1800;
+    public int kEverloopBaseAddress = 0x2000;
+    public int kGPIOBaseAddress = 0x2800;
+    public int kMCUBaseAddress = 0x3800;
+
+    // MCU offsets map
+    public int kMemoryOffsetUV = 0x00;
+    public int kMemoryOffsetPressure = 0x10;
+    public int kMemoryOffsetHumidity = 0x20;
+    public int kMemoryOffsetIMU = 0x30;
+    public int kMemoryOffsetMCU = 0x90;
+
+}
