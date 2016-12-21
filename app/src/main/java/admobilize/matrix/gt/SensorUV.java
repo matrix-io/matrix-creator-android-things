4fdb197194d3cf9e9c9c68de751c72bcf92c865f
Antonio Vanegas <hpsaturn@gmail.com>
2016-12-20 22:20:37 -0500

    add extension from SensorBase for memory maps

--- app/src/main/java/admobilize/matrix/gt/SensorUV.java
+++ app/src/main/java/admobilize/matrix/gt/SensorUV.java
@@ -1,26 +1,21 @@
 package admobilize.matrix.gt;
 
-import android.util.Log;
-
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
-import java.util.Arrays;
 
 /**
  * Created by Antonio Vanegas @hpsaturn on 12/20/16.
  */
 
-public class SensorUV {
-
-    private Wishbone wb;
+public class SensorUV extends SensorBase {
 
     public SensorUV(Wishbone wb) {
-        this.wb=wb;
+        super(wb);
     }
 
     public float read (){
         byte[] data = new byte[8];
-        wb.SpiRead((short) (0x3800+(0x00 >> 1)),data,4);
+        wb.SpiRead((short) (kMCUBaseAddress+(kMemoryOffsetUV >> 1)),data,4);
         return ByteBuffer.wrap(data).order(ByteOrder.LITTLE_ENDIAN).getFloat();
     }
 
