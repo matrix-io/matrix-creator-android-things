609a7882df4a991897a611f8a8b9be7758131a26
Antonio Vanegas <hpsaturn@gmail.com>
2017-01-02 16:17:48 -0500

    add main script for FPGA programing (need root)

---
+++ firmware/matrix-firmware-loader.sh
@@ -0,0 +1,28 @@
+#!/system/bin/sh
+echo -n "disable Matrix Creator microcontroller.."
+echo 18 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio18/direction
+echo 1 > /sys/class/gpio/gpio18/value
+echo 0 > /sys/class/gpio/gpio18/value
+echo 1 > /sys/class/gpio/gpio18/value
+echo "done"
+
+# enable LED and turn OFF
+echo 21 > /sys/class/gpio/export
+echo out > /sys/class/gpio/gpio21/direction
+echo 0 > /sys/class/gpio/gpio21/value
+
+
+echo "reconfigurate FPGA and Micro.."
+/system/bin/matrix-xc3sprog -p 1 -c sysfsgpio /system/bin/matrix_system.bit
+echo "done"
+
+# blink LED 
+echo 1 > /sys/class/gpio/gpio21/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio21/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio21/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio21/value
+
