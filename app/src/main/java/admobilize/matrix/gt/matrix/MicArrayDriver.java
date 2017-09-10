b35517fb1dfb9f4643956ff9b8a2e96898a1ac71
Antonio Vanegas <hpsaturn@gmail.com>
2017-09-10 12:56:25 -0500

    add AndroidThings User Driver for AudioManager

---
+++ app/src/main/java/admobilize/matrix/gt/matrix/MicArrayDriver.java
@@ -0,0 +1,93 @@
+package admobilize.matrix.gt.matrix;
+
+import android.media.AudioDeviceInfo;
+import android.media.AudioFormat;
+import android.util.Log;
+
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.userdriver.AudioInputDriver;
+import com.google.android.things.userdriver.UserDriverManager;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+import admobilize.matrix.gt.Config;
+
+
+/**
+ * Created by Antonio Vanegas @hpsaturn on 9/9/17.
+ */
+
+public class MicArrayDriver implements AutoCloseable {
+
+    private static final String TAG = MicArrayDriver.class.getSimpleName();
+    private static final boolean DEBUG = Config.DEBUG;
+
+    // buffer of 0.05 sec of sample data at 48khz / 16bit.
+    private static final int BUFFER_SIZE = 96000 / 20;
+    // buffer of 0.5 sec of sample data at 48khz / 16bit.
+    private static final int FLUSH_SIZE = 48000;
+
+    private final MicArray micArray;
+    private Gpio mTriggerGpio;
+    private AudioInputUserDriver mAudioInputDriver;
+
+    // Audio constants.
+    private static final String PREF_CURRENT_VOLUME = "current_volume";
+    private static final int SAMPLE_RATE = 16000;
+    private static final int ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+    private static final int DEFAULT_VOLUME = 100;
+
+    private static final AudioFormat AUDIO_FORMAT_STEREO =
+            new AudioFormat.Builder()
+                    .setChannelMask(AudioFormat.CHANNEL_IN_STEREO)
+                    .setEncoding(ENCODING)
+                    .setSampleRate(SAMPLE_RATE)
+                    .build();
+
+    private static final AudioFormat AUDIO_FORMAT_IN_MONO =
+            new AudioFormat.Builder()
+                    .setChannelMask(AudioFormat.CHANNEL_IN_MONO)
+                    .setEncoding(ENCODING)
+                    .setSampleRate(SAMPLE_RATE)
+                    .build();
+
+    public MicArrayDriver(MicArray micArray) {
+        this.micArray=micArray;
+    }
+
+    @Override
+    public void close() throws Exception {
+        unregisterAudioInputDriver();
+    }
+
+    private class AudioInputUserDriver extends AudioInputDriver {
+        @Override
+        public void onStandbyChanged(boolean b) {
+        }
+
+        @Override
+        public int read(ByteBuffer byteBuffer, int i) {
+            try {
+                return micArray.read(byteBuffer, i);
+            } catch (IOException e) {
+                Log.e(TAG, "error during read operation:", e);
+                return -1;
+            }
+        }
+    }
+
+    public void registerAudioInputDriver() {
+        mAudioInputDriver = new AudioInputUserDriver();
+        UserDriverManager.getManager().registerAudioInputDriver(mAudioInputDriver, AUDIO_FORMAT_STEREO,
+                AudioDeviceInfo.TYPE_BUILTIN_MIC, BUFFER_SIZE);
+    }
+
+    public void unregisterAudioInputDriver() {
+        if (mAudioInputDriver != null) {
+            UserDriverManager.getManager().unregisterAudioInputDriver(mAudioInputDriver);
+            mAudioInputDriver = null;
+        }
+    }
+
+}
